f69239d3c706075c132f2caeab5f741ffaa5a399
Ludovic <ludovic@localhost.localdomain>
2021-09-30 11:45:58 +0200

    revert params to gmcd

--- defaults/pref/pacome.js
+++ defaults/pref/pacome.js
@@ -10,19 +10,26 @@ pref("pacome.krbauth.whoami", "http://intranet.krb.gendarmerie.fr:14382/krbWhoAm
 pref("network.negotiate-auth.trusted-uris", ".krb.gendarmerie.fr, intranet.sso.gendarmerie.fr");
 
 //url du serveur pacome de parametrage
-//pref("pacome.urlparam", "https://pacome.s2.m2.e2.rie.gouv.fr/param.php");
-pref("pacome.urlparam", "http://localhost:14382/param.php");
-pref("pacome.urlparam.krb", "http://intranet.krb.gendarmerie.fr:14382/krbparam.php");
+pref("pacome.urlparam", "https://pacome.s2.m2.e2.rie.gouv.fr/param.php");
+
+//Gendarmerie ---
+//pref("pacome.urlparam", "http://localhost:14382/param.php");
+//pref("pacome.urlparam.krb", "http://intranet.krb.gendarmerie.fr:14382/krbparam.php");
 //url du serveur pacome de parametrage sÃ©curisÃ©e hors krb ?
-pref("pacome.urlparam.auth", true);
+//pref("pacome.urlparam.auth", true);
+//Gendarmerie ---
 
 //url du serveur pacome de verification de mot de passe
-//pref("pacome.urlmdp", "https://pacome.s2.m2.e2.rie.gouv.fr/pacomemdp2.php");
-pref("pacome.urlmdp", "http://localhost:14382/pacomemdp2.php");
+pref("pacome.urlmdp", "https://pacome.s2.m2.e2.rie.gouv.fr/pacomemdp2.php");
 
 //url du serveur pacome de changement de mot de passe
-//pref("pacome.chgmdp", "https://pacome.s2.m2.e2.rie.gouv.fr/pacomemdp2.php");
-pref("pacome.chgmdp", "http://localhost:14382/pacomemdp2.php");
+pref("pacome.chgmdp", "https://pacome.s2.m2.e2.rie.gouv.fr/pacomemdp2.php");
+
+
+//Gendarmerie ---
+//pref("pacome.urlmdp", "http://localhost:14382/pacomemdp2.php");
+//pref("pacome.chgmdp", "http://localhost:14382/pacomemdp2.php");
+//Gendarmerie ---
 
 //pacome v6 : version initiale du parametrage proxy
 pref("pacome.proxy.version", "14");
@@ -36,8 +43,12 @@ pref("pacome.aidemiseajour", "http://bureautique.metier.e2.rie.gouv.fr/supports/
 
 
 //url synchronisation des etiquettes
-pref("courrielleur.etiquettes.service", "http://localhost:14382/pacometags.php");
-pref("courrielleur.etiquettes.service.krb", "http://intranet.krb.gendarmerie.fr:14382/krbpacometags.php");
+pref("courrielleur.etiquettes.service", "https://pacome.s2.m2.e2.rie.gouv.fr/pacometags.php");
+
+//Gendarmerie ---
+//pref("courrielleur.etiquettes.service", "http://localhost:14382/pacometags.php");
+//pref("courrielleur.etiquettes.service.krb", "http://intranet.krb.gendarmerie.fr:14382/krbpacometags.php");
+//Gendarmerie ---
 
 // synchronisation automatique des etiquettes au demarrage (si true)
 pref("courrielleur.etiquettes.majauto", true);
